9e3f2590169a452fdd7a0e3f88fb8a158c19143a
Fleurys Nomenjanahary <115474618+fleurys26@users.noreply.github.com>
2024-01-26 15:29:21 +0300

    Leslie
    statfarany

---
+++ src/main/java/com/projetS5/back/controller/StatistiqueDatesController.java
@@ -0,0 +1,23 @@
+package com.projetS5.back.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetS5.back.model.StatistiqueDate;
+import com.projetS5.back.service.StatistiqueDateService;
+
+@RestController
+@RequestMapping("/StatDate")
+public class StatistiqueDatesController {
+    @Autowired
+    private StatistiqueDateService dateService;
+
+    @GetMapping("/date")
+    public java.util.List<StatistiqueDate> getAllDates() {
+        return dateService.getAllDate();
+    }
+
+    
+}
