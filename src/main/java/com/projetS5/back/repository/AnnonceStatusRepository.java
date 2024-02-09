5128ba2805b5e3c2cdf5662616310b831a1281a3
TojonirinaMandresy <tojoylek@gmail.com>
2024-02-10 01:35:51 +0300

    Last commit

--- src/main/java/com/projetS5/back/repository/AnnonceStatusRepository.java
+++ src/main/java/com/projetS5/back/repository/AnnonceStatusRepository.java
@@ -1,6 +1,5 @@
 package com.projetS5.back.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
